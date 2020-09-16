9b55c4621fce04cdbaa5b63f03f600d6f5c05ee5
Evan Helbig <evan.helbig@willowtreeapps.com>
2020-09-15 20:48:55 -0400

    Updating table schema

--- lib/shallwecook-api-stack.ts
+++ lib/shallwecook-api-stack.ts
@@ -1,4 +1,10 @@
-import { Stack, Construct, StackProps, RemovalPolicy } from "@aws-cdk/core";
+import {
+  Stack,
+  Construct,
+  StackProps,
+  RemovalPolicy,
+  SizeRoundingBehavior,
+} from "@aws-cdk/core";
 import { Table, AttributeType, BillingMode } from "@aws-cdk/aws-dynamodb";
 
 export class ShallwecookApiStack extends Stack {
@@ -8,9 +14,30 @@ export class ShallwecookApiStack extends Stack {
     const table = new Table(this, "recipes", {
       tableName: "recipes",
       partitionKey: { name: "category", type: AttributeType.STRING },
-      sortKey: { name: "diet", type: AttributeType.STRING },
+      sortKey: { name: "id", type: AttributeType.NUMBER },
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    table.addLocalSecondaryIndex({
+      indexName: "category-name-index",
+      sortKey: { name: "name", type: AttributeType.STRING },
+    });
+
+    table.addLocalSecondaryIndex({
+      indexName: "category-diet-index",
+      sortKey: { name: "diet", type: AttributeType.STRING },
+    });
+
+    table.addGlobalSecondaryIndex({
+      indexName: "diet-ingredients-index",
+      partitionKey: { name: "diet", type: AttributeType.STRING },
+    });
+
+    table.addGlobalSecondaryIndex({
+      indexName: "diet-allergen-index",
+      partitionKey: { name: "diet", type: AttributeType.STRING },
+      sortKey: { name: "allergen", type: AttributeType.STRING },
     });
   }
 }
