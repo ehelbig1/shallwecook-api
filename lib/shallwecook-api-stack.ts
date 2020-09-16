831d512a3aad5cb945cc8b4ac0a103bb0063cfae
Evan Helbig <evan.helbig@willowtreeapps.com>
2020-09-16 14:00:13 -0400

    Adding cognito user pool and identity pool

--- lib/shallwecook-api-stack.ts
+++ lib/shallwecook-api-stack.ts
@@ -4,8 +4,19 @@ import {
   StackProps,
   RemovalPolicy,
   SizeRoundingBehavior,
+  Duration,
 } from "@aws-cdk/core";
 import { Table, AttributeType, BillingMode } from "@aws-cdk/aws-dynamodb";
+import { Role, ServicePrincipal } from "@aws-cdk/aws-iam";
+import {
+  UserPoolIdentityProvider,
+  UserPoolIdentityProviderBase,
+  UserPool,
+  VerificationEmailStyle,
+  AccountRecovery,
+  UserPoolClientIdentityProvider,
+  UserPoolIdentityProviderAmazon,
+} from "@aws-cdk/aws-cognito";
 
 export class ShallwecookApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -38,6 +49,45 @@ export class ShallwecookApiStack extends Stack {
       indexName: "diet-allergen-index",
       partitionKey: { name: "diet", type: AttributeType.STRING },
       sortKey: { name: "allergen", type: AttributeType.STRING },
+    });
+
+    const userPool = new UserPool(this, "shallwecook-userpool", {
+      userPoolName: "shallwecook-userpool",
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: "Verify your email for ShallWeCook!",
+        emailBody:
+          "Hello {username}, Thanks for signing up to ShallWeCook! Your verification code is {####}",
+        emailStyle: VerificationEmailStyle.CODE,
+      },
+      signInAliases: {
+        email: true,
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+        requireUppercase: true,
+        tempPasswordValidity: Duration.days(3),
+      },
+      accountRecovery: AccountRecovery.EMAIL_ONLY,
+      emailSettings: {
+        from: "noreply@shallwecook.com",
+        replyTo: "support@shallwecook.com",
+      },
+    });
+
+    const client = userPool.addClient("shallwecook", {
+      supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
+    });
+
+    const clientId = client.userPoolClientId;
+
+    userPool.addDomain("ShallWeCookDomain", {
+      cognitoDomain: {
+        domainPrefix: "shallwecook",
+      },
     });
   }
 }
